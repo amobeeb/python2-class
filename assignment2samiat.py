302c9f5a1339075e85c49837d5c8cb982f19f4a7
SamiatOluwapelumi <103038472+SamiatOluwapelumi@users.noreply.github.com>
2022-07-06 23:47:13 +0530

    push

---
+++ assignment2samiat.py
@@ -0,0 +1,14 @@
+import cmath
+num = 8990.892002
+format_num = "{:.2f}".format(num)
+print(format_num)
+
+
+
+a=4
+b= -11
+c=-21
+d=(b**2)-(4*a*c)
+ans1=(-b+ cmath.sqrt(d)/(2*a))
+ans2=(-b- cmath.sqrt(d)/(2*a))
+print('the roots are ', (ans1 , ans2) )
