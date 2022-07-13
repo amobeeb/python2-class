2d06ead48507bd6ce2916eb0257362e8f9d49b6f
SamiatOluwapelumi <103038472+SamiatOluwapelumi@users.noreply.github.com>
2022-07-14 01:09:00 +0530

    commit

--- assignment3samiat.py
+++ assignment3samiat.py
@@ -1,21 +1,35 @@
 
 import math
+from unicodedata import decimal
 #this program will print the area of a regular polygon
 #s=lenght of a side
 #n=number of sides
 #apothem=s/(2tan(180/n))
 #perimeter=s*n
 #AreaOfPolygon=(perimeter*apothem)/2
-s=float(input('lenght of a side: '))
-n=float(input('number of sides: '))
-perimeter=s*n
-a=180/n
-b=math.tan(a)
-apothem=s/(2*b)
-AreaOfPolygon=(perimeter*apothem)/2
-print(a)
-print(b)
-print(perimeter)
-print(apothem)
-print(AreaOfPolygon)
 
+
+# s=float(input('lenght of a side: '))
+# n=float(input('number of sides: '))
+# perimeter=s*n
+# a=180/n
+# b=math.tan(a)
+# apothem=s/(2*b)
+# AreaOfPolygon=(perimeter*apothem)/2
+# print(a)
+# print(b)
+# print(perimeter)
+# print(apothem)
+# print(AreaOfPolygon)
+
+#This program will convert binary number to decimal number.
+
+
+b_num = list(input("Input a binary number: "))
+value = 0
+
+for i in range(len(b_num)):
+	digit = b_num.pop()
+	if digit == '1':
+		value = value + pow(2, i)
+print("The decimal value of the number is", value)
