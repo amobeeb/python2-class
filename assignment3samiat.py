3520ad880cf4acef09214e1d94512634ac7cf04e
SamiatOluwapelumi <103038472+SamiatOluwapelumi@users.noreply.github.com>
2022-07-14 01:13:43 +0530

    commit

--- assignment3samiat.py
+++ assignment3samiat.py
@@ -6,24 +6,21 @@ from unicodedata import decimal
 #n=number of sides
 #apothem=s/(2tan(180/n))
 #perimeter=s*n
-#AreaOfPolygon=(perimeter*apothem)/2
-
-
-# s=float(input('lenght of a side: '))
-# n=float(input('number of sides: '))
-# perimeter=s*n
-# a=180/n
-# b=math.tan(a)
-# apothem=s/(2*b)
 # AreaOfPolygon=(perimeter*apothem)/2
-# print(a)
-# print(b)
-# print(perimeter)
-# print(apothem)
-# print(AreaOfPolygon)
+s=float(input('lenght of a side: '))
+n=float(input('number of sides: '))
+perimeter=s*n
+a=180/n
+b=math.tan(a)
+apothem=s/(2*b)
+AreaOfPolygon=(perimeter*apothem)/2
+print(a)
+print(b)
+print(perimeter)
+print(apothem)
+print(AreaOfPolygon)
 
 #This program will convert binary number to decimal number.
-
 
 b_num = list(input("Input a binary number: "))
 value = 0
