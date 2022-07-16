44c4b4c240914b0f3b4a8978ab297c0059b94c41
SamiatOluwapelumi <103038472+SamiatOluwapelumi@users.noreply.github.com>
2022-07-16 22:09:36 +0530

    commit

--- assignment3samiat.py
+++ assignment3samiat.py
@@ -7,18 +7,18 @@ from unicodedata import decimal
 #apothem=s/(2tan(180/n))
 #perimeter=s*n
 # AreaOfPolygon=(perimeter*apothem)/2
-s=float(input('lenght of a side: '))
-n=float(input('number of sides: '))
-perimeter=s*n
-a=180/n
-b=math.tan(a)
-apothem=s/(2*b)
-AreaOfPolygon=(perimeter*apothem)/2
-print(a)
-print(b)
-print(perimeter)
-print(apothem)
-print(AreaOfPolygon)
+# s=float(input('lenght of a side: '))
+# n=float(input('number of sides: '))
+# perimeter=s*n
+# a=180/n
+# b=math.tan(a)
+# apothem=s/(2*b)
+# AreaOfPolygon=(perimeter*apothem)/2
+# # print(a)
+# print(b)
+# print(perimeter)
+# print(apothem)
+# print(AreaOfPolygon)
 
 #This program will convert binary number to decimal number.
 
@@ -27,6 +27,9 @@ value = 0
 
 for i in range(len(b_num)):
 	digit = b_num.pop()
-	if digit == '1':
-		value = value + pow(2, i)
-print("The decimal value of the number is", value)
+	print(digit)
+	# if b_num[i] == '1':
+        #  print(digit)
+		# value = value + pow(2, i)
+		# print(value)
+# print("The decimal value of the number is", value)
